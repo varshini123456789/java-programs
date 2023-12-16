bcf395fa027ec329a67b446172dfed07c7a09971
varshini123456789 <153972215+varshini123456789@users.noreply.github.com>
2023-12-16 11:31:55 +0530

    Add files via upload

---
+++ wednesday.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class wednesday
+	{
+		public static void main(String args[])
+		{
+			System.out.println("Enter the total numbers: ");
+			Scanner reader = new Scanner(System.in);
+			int number = reader.nextInt();
+			System.out.println("First n natural numbers are -------------------");
+			for (int i = 1; i <= number; i++)
+				{
+					System.out.println(i);
+				}
+		}
+
+	}
